7d312003deb0bea7a937e6587be85c58498dab31
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-03 17:26:46 -0300

    Component scan with XML tag

--- src/main/java/guru/springframework/springboot/DiexampleApplication.java
+++ src/main/java/guru/springframework/springboot/DiexampleApplication.java
@@ -3,12 +3,13 @@ package guru.springframework.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import guru.springframework.controllers.GreetingController;
 
 @SpringBootApplication
-@ComponentScan("guru.springframework")
+//@ComponentScan("guru.springframework")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DiexampleApplication {
 
 	public static void main(String[] args) {
