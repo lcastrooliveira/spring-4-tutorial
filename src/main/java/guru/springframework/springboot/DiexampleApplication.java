ebb87ef074f0030d57fd308818ce41f145ccdd39
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-03 15:21:43 -0300

    Use of @ComponentScan annotation

--- src/main/java/guru/springframework/springboot/DiexampleApplication.java
+++ src/main/java/guru/springframework/springboot/DiexampleApplication.java
@@ -1,12 +1,14 @@
-package guru.springframework;
+package guru.springframework.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import guru.springframework.controllers.GreetingController;
 
 @SpringBootApplication
+@ComponentScan("guru.springframework")
 public class DiexampleApplication {
 
 	public static void main(String[] args) {
